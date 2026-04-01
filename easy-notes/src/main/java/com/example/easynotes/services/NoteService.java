acb546c4bf12e95450efb2e8479e8ff7ef0000d6
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-01 21:23:12 +0200

    feat: implementa inserimento automatico della data odierna, qualora il parametro createdAt sia null nel metodo aggiungiNota(...)

--- easy-notes/src/main/java/com/example/easynotes/services/NoteService.java
+++ easy-notes/src/main/java/com/example/easynotes/services/NoteService.java
@@ -2,11 +2,16 @@ package com.example.easynotes.services;
 
 import com.example.easynotes.dto.AggiungiNotaRequest;
 import com.example.easynotes.dto.AggiungiNotaResponse;
+import com.example.easynotes.dto.NotaResponse;
 import com.example.easynotes.model.Note;
 import com.example.easynotes.repositories.NoteRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class NoteService {
@@ -18,6 +23,10 @@ public class NoteService {
     private ModelMapper modelMapper;
 
     public AggiungiNotaResponse aggiungiNota(AggiungiNotaRequest request) {
+        if (request.getCreatedAt() == null) {
+            Date createdAt = Date.valueOf(LocalDate.now());
+            request.setCreatedAt(createdAt);
+        }
         Note nota = modelMapper.map(request, Note.class);
         nota = noteRepository.save(nota);
         return modelMapper.map(nota, AggiungiNotaResponse.class);
